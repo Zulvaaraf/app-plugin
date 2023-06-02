bf34c6d2d147ec5d8d828e67dab89bd2f8dcb5d8
zulvaaraf <zulvasebelas@gmail.com>
2023-06-02 23:21:26 +0700

    make note service

--- src/service/inMemory/NotesService.js
+++ src/service/inMemory/NotesService.js
@@ -27,4 +27,45 @@ class NotesService {
     }
     return id;
   }
+
+  getNote() {
+    return this._notes;
+  }
+
+  getNoteById(id) {
+    const note = this._notes.filter((n) => n.id === id)[0];
+    if (!note) {
+      throw new Error('Catatan tidak ditemukan');
+    }
+    return note;
+  }
+
+  editNoteById(id, { title, body, tags }) {
+    const index = this._notes.findIndex((n) => n.id === id);
+
+    if (index === -1) {
+      throw new Error('Gagal memperbarui catatan. Id tidak ditemukan');
+    }
+
+    const updatedAt = new Date().toISOString();
+    this._notes[index] = {
+      ...this._notes[index],
+      title,
+      body,
+      tags,
+      updatedAt,
+    };
+  }
+
+  deleteNoteById(id) {
+    const index = this._notes.findIndex((n) => n.id === id);
+
+    if (index === -1) {
+      throw new Error('Catatan gagal dihapus, Id tidak ditemukan');
+    }
+
+    this._notes.splice(index, 1);
+  }
 }
+
+module.exports = NotesService;

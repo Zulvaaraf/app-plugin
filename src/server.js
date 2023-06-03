8c7ba0b8a694b7ddb5b4ec7c7cad8bf14bbb6b63
zulvaaraf <zulvasebelas@gmail.com>
2023-06-04 00:04:13 +0700

    inital commit

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
@@ -11,8 +10,6 @@ const init = async () => {
       },
     },
   });
-
-  server.route(routes);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);

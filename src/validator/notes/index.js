afb32d5eb07160a78562b22ae5a838d90c418ace
zulvaaraf <zulvasebelas@gmail.com>
2023-06-06 23:19:59 +0700

    data commit validation

---
+++ src/validator/notes/index.js
@@ -0,0 +1,10 @@
+const { NotePayloadSchema } = require('./schema');
+
+const NotesValidator = (payload) => {
+  const validationResult = NotePayloadSchema.validate(payload);
+  if (validationResult.error) {
+    throw new Error(validationResult.error.message);
+  }
+};
+
+module.exports = NotesValidator;

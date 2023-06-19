08275dde37da4872642b8df2de52eb9b948b830b
zulvaaraf <zulvasebelas@gmail.com>
2023-06-20 03:16:55 +0700

    authentication implemented

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesServices');
@@ -27,6 +28,30 @@ const init = async () => {
         origin: ['*'],
       },
     },
+  });
+
+  // Plugin Eksternal
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  // Mendefinisikan strategy authentikasi Jwt
+  server.auth.strategy('notesapp_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
 
   await server.register([
